3dfcb2e20ca0fb79dcd94f2d26c39a8005347bcb
Aleftos <aleftos@outlook.com>
2016-11-28 22:00:58 -0400

    Update moment-date-formatter.ts

--- app/moment-date-formatter.ts
+++ app/moment-date-formatter.ts
@@ -8,6 +8,5 @@ declare var moment: any;
 export class DateFormatter {
     public format(date:Date, format:string):string {
         return moment(date.getTime()).format(format);
-        //return  date.getTime().toString() ;
     }
 }
